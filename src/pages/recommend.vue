eb5408cc943647cb7a554501932a192d818836a2
180810 <971001>
2021-05-06 09:07:23 +0800

    修复异常

--- src/pages/recommend.vue
+++ src/pages/recommend.vue
@@ -1,18 +1,15 @@
 <template>
   <div style="padding: 10px 30px;user-select: none;">
     <Personalized />
-    <MiniCover style="width: 500px;height: 500px"/>
   </div>
 </template>
 <script lang="ts">
 import Personalized from '@/components/Personalized.vue'
 import { getQRCodeKey } from '../api/user'
-import MiniCover from '@/components/MiniCover.vue'
 
 export default {
   components: {
-    Personalized,
-    MiniCover
+    Personalized
   },
   created() {
     // this.test()
