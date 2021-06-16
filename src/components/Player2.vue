e3f563ff0c9784bf16d51a1bda9a34d957eed0b5
FuYH <1351330877@qq.com>
2021-06-16 23:15:01 +0800

    引入howler.js

---
+++ src/components/Player2.vue
@@ -0,0 +1,25 @@
+<template />
+
+<script>
+import { defineComponent, computed, watch, ref } from 'vue'
+import { Howl, Howler } from 'howler'
+export default {
+  name: 'Player2',
+  setup() {
+    debugger
+    const sound = new Howl({
+      src: ['https://music.163.com/song/media/outer/url?id=1811459887.mp3'],
+      html5: true
+    })
+
+    sound.play()
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
