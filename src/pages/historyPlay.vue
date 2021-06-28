cf57d3ee71ce2a0c3881d63d16a16c4c22e3eb78
180810 <971001>
2021-06-28 16:47:36 +0800

    修改最近播放页面

--- src/pages/historyPlay.vue
+++ src/pages/historyPlay.vue
@@ -1,34 +1,27 @@
 <template>
   <div style="user-select: none;padding: 20px 30px">
     <span class="h1">最近播放</span>
-    <div class="selections">
-      <div @click="select=!select" style="cursor: pointer">
-        <span :style="{color: select?'var(--primary-color)':'#000000'}">歌曲</span>
-        <div class="line" v-if="select"/>
-      </div>
-      <div @click="select=!select" style="cursor: pointer">
-        <span :style="{color: !select?'var(--primary-color)':'#000000'}">视频</span>
-        <div class="line" v-if="!select"/>
-      </div>
-    </div>
+    <Tabs :tab-list="tabList" style="margin-bottom: 20px"/>
     <div>
-      <button class="playAll"><svg-icon name="listPlay"/> 播放全部</button>
+      <button class="playAll"><svg-icon name="listPlay" /> 播放全部</button>
     </div>
-    <MusicList :data="detailList" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import MusicList from '@/components/MusicList.vue'
+import Tabs from '@/components/Tabs.vue'
 export default defineComponent({
   name: 'HistoryPlay',
   components: {
-    MusicList
+    Tabs
   },
   data() {
     return {
-      select: true
+      tabList: [
+        { title: '歌曲12', name: 'Featured' },
+        { title: '视频', name: 'Leaderboard' }
+      ]
     }
   },
   computed: {
@@ -40,24 +33,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.selections{
-  width: 120px;
-  margin: 10px 0 20px 0;
-  font-size: 18px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.line{
-  height: 4px;
-  width: 25px;
-  background: var(--primary-color);
-  border-radius: 10px;
-  margin-top: 3px;
-  margin-left: 50%;
-  transform: translateX(-50%);
-  transition: 1s;
-}
 .playAll{
   cursor: pointer;
   border: none;
