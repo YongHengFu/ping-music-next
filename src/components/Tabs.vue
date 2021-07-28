1ade0d61e7a8f3ad4279a9910335a3b711a233a8
FuYH <1351330877@qq.com>
2021-07-28 22:41:47 +0800

    完善Tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -4,7 +4,7 @@
       v-for="(item,ind) of tabList"
       :key="item.name"
       class="pane"
-      :style="ind===index?'color:var(--primary-color)':'color: rgb(69, 74, 80);'"
+      :style="ind===index?'color:var(--primary-color);font-size:18px;':'color: rgb(69, 74, 80);'"
       @click="change(ind,item.name)"
     >{{ item.title }} </div>
     <div class="slider" :style="'margin-left:'+left+'px'">
@@ -14,16 +14,25 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref, watch } from 'vue'
 export default defineComponent({
   name: 'Tabs',
   props: {
-    tabList: Object
+    tabList: Array,
+    tabName: String
   },
   setup(props, ctx) {
     const index = ref(0)
     const left = ref(0)
     const translateX = computed(() => (80 - (props?.tabList[0]?.title.replace(/[^\x00-\xff]/g, '01').length) * 8) / 2)
+    watch(props, () => {
+      for (const [index, item] of props?.tabList.entries()) {
+        if (item.name === props.tabName) {
+          change(index, props.tabName)
+          break
+        }
+      }
+    })
     const change = (ind:number, name:string) => {
       if (ind !== index.value) {
         index.value = ind
